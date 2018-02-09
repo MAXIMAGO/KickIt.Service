5baf97d17dad5a55ef2bfff6437e880181084352
Maik <maik.schoeneich@maximago.de>
2018-02-09 22:16:34 +0100

    Initial Commit

---
+++ src/MAXIMAGO.KickIT/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MAXIMAGO.KickIT
+{
+    public class Startup
+    {
+        public const string ALLOW_ALL_POLICY_NAME = "AllowAll";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // add CORS configuration
+            services.AddCors(builder =>
+                builder.AddPolicy(ALLOW_ALL_POLICY_NAME,
+                policyBuilder => policyBuilder.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()));
+
+            services.AddMvc();
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(ALLOW_ALL_POLICY_NAME);
+            app.UseMvc();
+        }
+    }
+}
